3fe6add818a0adff964c05a6764e24af81a8a190
pujanjoci <pujanjoci2@gmail.com>
2025-11-19 16:34:04 +0545

    for gh:pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",                     // REQUIRED for GitHub Pages
-  images: {
-    unoptimized: true,                  // Needed because images optimizer won't run on static hosting
-  },
-  basePath: "/the-grand",               // MUST match your GitHub repo name
-  assetPrefix: "/the-grand/",           // Static assets prefix
+  output: "export",
+  basePath: "/the-grand",
+  assetPrefix: "/the-grand/",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
